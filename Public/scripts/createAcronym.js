6ac3f952d80cebc167fb336622d2fac4a985354d
Tim <0xtimc@gmail.com>
2018-05-02 12:38:55 +0100

    Update packages

--- Public/scripts/createAcronym.js
+++ Public/scripts/createAcronym.js
@@ -1,7 +1,7 @@
 $.ajax({
   url: "/api/categories/",
-  type: 'GET',
-  contentType: 'application/json; charset=utf-8'
+  type: "GET",
+  contentType: "application/json; charset=utf-8"
 }).then(function (response) {
     var dataToReturn = [];
     for (var i=0; i < response.length; i++) {
