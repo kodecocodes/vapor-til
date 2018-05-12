c93673ac890a29632a9543dd4d46f6989a788c9d
Tim <0xtimc@gmail.com>
2018-05-12 20:18:09 +0100

    Bring back admin user

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -92,7 +92,7 @@ public func configure(
   migrations.add(model: Category.self, database: .psql)
   migrations.add(model: AcronymCategoryPivot.self, database: .psql)
   migrations.add(model: Token.self, database: .psql)
-//  migrations.add(migration: AdminUser.self, database: .psql)
+  migrations.add(migration: AdminUser.self, database: .psql)
   services.register(migrations)
 
   // Configure the rest of your application here
